a6f104eccf56202e59534bbbc42411022debe716
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-06-30 21:37:36 +0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import "./index.css";
 
 import App from "./App";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./Redux/rootReducer";
 
+// setup redux
+const store = createStore(rootReducer);
+
+// const store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
